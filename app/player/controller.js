18f6a1634ad9c81d30cf91fa05d50bcafc1ff35c
Mohamad Farhan <farhannmuh26@gmail.com>
2022-02-14 17:16:56 +0700

    Bug Fixing

--- app/player/controller.js
+++ app/player/controller.js
@@ -22,7 +22,7 @@ module.exports = {
     try {
       const { id } = req.params;
       const voucher = await Voucher.findOne({ _id: id }).populate("category").populate("nominals").populate("user", "_id name phoneNumber");
-      const payment = await Payment.find().populate("banks");
+      const payment = await Payment.find().populate("bank");
 
       if (!voucher) {
         return res.status(404).json({ message: "Voucher Not Found" });
